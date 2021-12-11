565c5de48ec809cd550026a7f6448217d3d65393
Ranka <darkigfx@gmail.com>
2021-12-11 14:23:37 +0100

    cleared level 05-A

---
+++ 05-A.py
@@ -0,0 +1,41 @@
+
+def mark(board, start:tuple, end:tuple):
+    """
+    marks horizontal and vertical lines in the board
+    """
+    if start[0] == end[0]:
+        low, high = sorted([start[1], end[1]]) # make sure to use the right range
+        y = start[0]
+        for i in range(low, high+1):
+            board[i][y] += 1
+
+    elif start[1] == end[1]:
+        low, high = sorted([start[0], end[0]])
+        x = start[1]
+        for i in range(low, high+1):
+            board[x][i] += 1
+
+def count_danger(board):
+    """
+    count all fields with more than 2 overlapping lines.
+    """
+    counter = 0
+    for i in range(0, len(board)):
+        for j in range(0, len(board)):
+            if board[i][j] >= 2:
+                counter += 1
+    return counter
+
+board = [[0 for x in range(1000)] for y in range(1000)] # just use 1000 here. we could technically calculate this. but there is no need to.
+
+with open("05-input.txt") as file:
+    lines = [x.strip("\n") for x in file.readlines()]
+    for line in lines:
+        splitline = line.split("->")
+        x = tuple(map(int, splitline[0].split(",")))
+        y = tuple(map(int, splitline[1].split(",")))
+        mark(board, x, y)
+
+    for line in board:
+        print("".join([str(i) for i in line]))
+print(count_danger(board))
